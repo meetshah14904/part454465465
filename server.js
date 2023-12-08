053f6981526becde839fdb6ad70ed2d5280a988e
meetshah14904 <88265089+meetshah14904@users.noreply.github.com>
2023-12-08 18:02:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ mongoose.connect(config.mongoUri, {
 });
 
 // Request logging middleware
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 // Existing route handlers...

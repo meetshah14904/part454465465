f6d00c27a547d537545c27979c84e98f2fe7692a
meetshah14904 <88265089+meetshah14904@users.noreply.github.com>
2023-12-08 17:50:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,38 @@
-/*const express = require("express");
-const path = require("path");
-const app = express();
-const assetsRouter = require("./server/assets-router");
-app.use("/src", assetsRouter);
-app.use("/", express.static(path.join(__dirname, "public")));
-app.get("/api/v1", (req, res) => {
-  res.json({
-    project: "React and Express Boilerplate",
-    from: "Vanaldito",
-  });
+import config from './config/config.js'; 
+import app from './server/express.js';
+import mongoose from 'mongoose';
+import express from 'express';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("Connected to the database!");
+})
+.catch(err => {
+  console.error(`Error connecting to the database. ${err}`);
 });
-app.get("/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-})
-const { PORT = 5000 } = process.env;
-app.listen(PORT, () => {
-  console.log();
-  console.log(`  App running in port ${PORT}`);
-  console.log();
-  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
-});*/
 
-/*import config from './config/config.js' 
-import app from './server/express.js'
-app.get("/", (req, res) => {
-res.json({ message: "Welcome to User application." });
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
 });
-app.listen(config.port, (err) => { 
-if (err) {
-console.log(err) 
-}
-console.info('Server started on port %s.', config.port) 
-})*/
 
-import config from './config/config.js' 
-import app from './server/express.js'
-import mongoose from 'mongoose' 
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true,
-//useCreateIndex: true, 
-useUnifiedTopology: true } )
+// Existing route handlers...
 
- .then(() => {
-console.log("Connected to the database!");
-})
-
-mongoose.connection.on('error', () => {
-throw new Error(`unable to connect to database: ${config.mongoUri}`) 
-})
-/*
-app.get("/", (req, res) => {
-res.json({ message: "Welcome to User application." });
+// Catch 404 and log it
+app.use((req, res, next) => {
+  console.error(`404 error. Resource not found: ${req.url}`);
+  res.status(404).send('Resource not found');
 });
-*/
-app.listen(config.port, (err) => { 
-if (err) {
-console.log(err) 
-}
-console.info('Server started on port %s.', config.port) 
-})
 
+app.listen(config.port, (err) => {
+  if (err) {
+    console.error(`Error starting server: ${err}`);
+  } else {
+    console.info(`Server started on port ${config.port}`);
+  }
+});

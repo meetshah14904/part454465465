f3d00e257b35f6b11213a43844fcf419cda24fb7
meetshah14904 <88265089+meetshah14904@users.noreply.github.com>
2023-12-08 17:06:19 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -11,10 +11,11 @@ process.env.MONGO_HOST ||
 export default config*/
 
 const config = {
-  env: "development",
-  port: 3000,
-  jwtSecret: "YOUR_secret_key",
-  mongoUri:
-    "mongodb+srv://meet:meet14@cluster0.yqvokoa.mongodb.net/meet",
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,  // This will use the PORT environment variable if available, otherwise default to 3000
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGODB_URI || "mongodb+srv://meet:meet14@cluster0.yqvokoa.mongodb.net/meet"  // Replace with your MongoDB URI
 };
+
 export default config;
+
